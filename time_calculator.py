3868c8f004f70f5b40f2b269a5fc26fe1e07c073
Ezequiel Manuel Ferraris <95259554+EzequielFerraris@users.noreply.github.com>
2022-05-08 22:15:28 -0300

    Update time_calculator.py

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,30 @@
-def add_time(start, duration):
+#MAIN FUNCTION
+def add_time(start, duration, starting_day = None):
+  
+#START VARIABLES
+    min_start = 0
+    hours_start = 0
+    daytime = ''
+    days_passed = 0
 
+#ACCUMULATIVE VARIABLES
+    min_sum = 0
+    hours_sum = 0
+    daytime_sum = 0
 
+#GET THE STARTING DATA AND TURN INTO NUMBERS
+  
+    temp1 = start.split(' ')
+    daytime = temp1[1]
 
+    temp2 = temp1[0].split(':')
+    min_start = int(temp2[1])
+    hours_start = int(temp2[0])
 
+#GET THE ADDING DATA AND TURN INTO NUMBERS
 
-    return new_time
+    temp3 = duration.split(':')
+    min_sum += int(temp3[1])
+    hours_sum += int(temp3[0])
+    
+#ADD HOURS AND MINUTES
